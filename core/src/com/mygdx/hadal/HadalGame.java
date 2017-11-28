dd87d3d32af6f8114a8c4aa3344b2ef674a1a410
Donpommelo <donpommelo@gmail>
2017-11-27 20:03:00 -0600

    test

---
+++ core/src/com/mygdx/hadal/HadalGame.java
@@ -0,0 +1,39 @@
+package com.mygdx.hadal;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class HadalGame extends ApplicationAdapter {
+	private static SpriteBatch batch;
+	private static int DEFAULT_WIDTH = 1600;
+	private static int DEFAULT_HEIGHT = 900;
+	
+	public static int CONFIG_WIDTH;
+	public static int CONFIG_HEIGHT;
+	
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
