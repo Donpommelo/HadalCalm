767da4e24d9eb79414c2c2c214533d9b0cb27e92
Donpommelo <donpommelo@gmail>
2018-02-18 17:32:56 -0600

    more ui stuff

--- core/src/com/mygdx/hadal/actors/UIPlay.java
+++ core/src/com/mygdx/hadal/actors/UIPlay.java
@@ -20,12 +20,21 @@ public class UIPlay extends AHadalActor{
 	
 	private TextureAtlas atlas;
 	
-	private TextureRegion main, reloading, hp, fuel, fuelCutoff;
+	private TextureRegion main, reloading, hp, hpLow, hpMissing, fuel, fuelCutoff;
 	private Array<AtlasRegion> itemNull, itemSelect, itemUnselect;
 	
 	private float scale = 0.75f;
 	private static final int x = 0;
 	private static final int y = 0;
+	
+	private static final float hpCatchup = 0.01f;
+	private static final float hpLowThreshold = 0.20f;
+	private static final float blinkCd = 0.1f;
+	
+	private float hpDelayed = 1.0f;
+	private boolean blinking = false;
+	private float blinkCdCount = 0.0f;
+	
 	
 	public UIPlay(AssetManager assetManager, PlayState state, Player player) {
 		super(assetManager);
@@ -38,6 +47,8 @@ public class UIPlay extends AHadalActor{
 		this.reloading = atlas.findRegion("UI_main_reloading");
 		this.reloading = atlas.findRegion("UI_main_reloading");
 		this.hp = atlas.findRegion("UI_main_healthbar");
+		this.hpLow = atlas.findRegion("UI_main_health_low");
+		this.hpMissing = atlas.findRegion("UI_main_healthmissing");
 		this.fuel = atlas.findRegion("UI_main_fuelbar");
 		this.fuelCutoff = atlas.findRegion("UI_main_fuel_cutoff");
 		this.itemNull = atlas.findRegions("UI_main_null");
@@ -50,13 +61,39 @@ public class UIPlay extends AHadalActor{
     public void draw(Batch batch, float alpha) {
 		batch.setProjectionMatrix(state.hud.combined);
 
+		//Calc the ratios needed to draw the bars
 		float hpRatio = player.getPlayerData().currentHp / player.getPlayerData().getMaxHp();
 		float fuelRatio = player.getPlayerData().currentFuel / player.getPlayerData().getMaxFuel();
 		float fuelCutoffRatio = player.getPlayerData().airblastCost * 
 				(1 + player.getPlayerData().getBonusAirblastCost()) / player.getPlayerData().getMaxFuel();
 		
+		//This code makes the hp bar delay work.
+		if (hpDelayed > hpRatio) {
+			hpDelayed -= hpCatchup;
+		} else {
+			hpDelayed = hpRatio;
+		}
+		
+		batch.draw(hpMissing, x + 233, y + 78, hp.getRegionWidth() * scale * hpDelayed, hp.getRegionHeight() * scale);
 		batch.draw(hp, x + 233, y + 78, hp.getRegionWidth() * scale * hpRatio, hp.getRegionHeight() * scale);
 		batch.draw(fuel, x + 233, y + 32, fuel.getRegionWidth() * scale * fuelRatio, fuel.getRegionHeight() * scale);
+		
+		//This makes low Hp indicator blink at low health
+		if (hpRatio <= hpLowThreshold) {
+			
+			blinkCdCount -= 0.01f;
+			
+			if (blinkCdCount < 0) {
+				blinking = !blinking;
+				blinkCdCount = blinkCd;
+			}
+		} else {
+			blinking = false;
+		}
+		
+		if (blinking) {
+			batch.draw(hpLow, x, y, main.getRegionWidth() * scale, main.getRegionHeight() * scale);
+		}
 		
 		batch.draw(main, x, y, main.getRegionWidth() * scale, main.getRegionHeight() * scale);
 		
