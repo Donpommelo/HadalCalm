421bf2b3460b07aa95522331e91e7ae1a526d662
Donpommelo <donpommelo@gmail>
2018-10-02 12:21:03 -0500

    particles

--- core/src/com/mygdx/hadal/audio/MusicPlayer.java
+++ core/src/com/mygdx/hadal/audio/MusicPlayer.java
@@ -31,74 +31,74 @@ public class MusicPlayer {
         }
 	 }
 	 
-	 // Sets the current song.
-	    public int setSong(String name){
-	        currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByName.get(name)));
-	        currentSong.setLooping(true);
-	        return 0;
-	    }
+	// Sets the current song.
+    public int setSong(String name){
+        currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByName.get(name)));
+        currentSong.setLooping(true);
+        return 0;
+    }
 
-	    // Load and lay a non-tracklist song.
-	    public void playSong(String name, float volume){
-	        if (currentSong != null){
-	            currentSong.stop();
-	        }
-	        currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByName.get(name)));
-	        currentSong.setVolume(volume);
-	        currentSong.play();
-	    }
+    // Load and lay a non-tracklist song.
+    public void playSong(String name, float volume){
+        if (currentSong != null){
+            currentSong.stop();
+        }
+        currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByName.get(name)));
+        currentSong.setVolume(volume);
+        currentSong.play();
+    }
 
-	    // Play next song.
-	    public void playNext(){
-	        if (currentSong == null || currentTrackNumber == -1 || currentTrackNumber == trackListByNumber.size() - 1){
-	            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(0)));
-	            currentTrackNumber = 0;
-	        } else {
-	            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(currentTrackNumber++)));
-	            currentTrackNumber++;
-	        }
-	    }
+    // Play next song.
+    public void playNext(){
+        if (currentSong == null || currentTrackNumber == -1 || currentTrackNumber == trackListByNumber.size() - 1){
+            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(0)));
+            currentTrackNumber = 0;
+        } else {
+            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(currentTrackNumber++)));
+            currentTrackNumber++;
+        }
+    }
 
-	    // Play previous song.
-	    public void playPrevious(){
-	        if (currentSong == null || currentTrackNumber == -1 || currentTrackNumber == 0){
-	            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(trackListByNumber.size() - 1)));
-	            currentTrackNumber = trackListByNumber.size() - 1;
-	        } else {
-	            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(currentTrackNumber--)));
-	            currentTrackNumber--;
-	        }
-	    }
+    // Play previous song.
+    public void playPrevious(){
+        if (currentSong == null || currentTrackNumber == -1 || currentTrackNumber == 0){
+            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(trackListByNumber.size() - 1)));
+            currentTrackNumber = trackListByNumber.size() - 1;
+        } else {
+            currentSong = Gdx.audio.newMusic(Gdx.files.internal(trackListByNumber.get(currentTrackNumber--)));
+            currentTrackNumber--;
+        }
+    }
 
-	    // Resumes the current song.
-	    public void play(){
-	        if (currentSong != null){
-	            currentSong.play();
-	        }
-	    }
+    // Resumes the current song.
+    public void play(){
+        if (currentSong != null){
+            currentSong.play();
+        }
+    }
 
-	    // Pauses the current song.
-	    public void pause(){
-	        if (currentSong != null){
-	            currentSong.pause();
-	        }
-	    }
+    // Pauses the current song.
+    public void pause(){
+        if (currentSong != null){
+            currentSong.pause();
+        }
+    }
 
-	    // Stops the current song.
-	    public void stop(){
-	        if (currentSong != null){
-	            currentSong.stop();
-	        }
-	    }
+    // Stops the current song.
+    public void stop(){
+        if (currentSong != null){
+            currentSong.stop();
+        }
+    }
 
-	    public void setVolume(float vol) {
-	        if (currentSong != null) {
-	            currentSong.setVolume(vol);
-	        }
-	    }
+    public void setVolume(float vol) {
+        if (currentSong != null) {
+            currentSong.setVolume(vol);
+        }
+    }
 
-	    // Clean up and dispose of player. Called when game closes.
-	    public void dispose(){
-	        currentSong.dispose();
-	    }
+    // Clean up and dispose of player. Called when game closes.
+    public void dispose(){
+        currentSong.dispose();
+    }
 }
