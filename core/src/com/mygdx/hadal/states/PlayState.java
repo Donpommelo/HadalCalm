f25cba2660a1c8b5c0b0da6ec29d763fbc4e3733
Donpommelo <donpommelo@gmail>
2019-04-06 17:11:33 -0500

    fixed client walls disappearing on death

--- core/src/com/mygdx/hadal/states/PlayState.java
+++ core/src/com/mygdx/hadal/states/PlayState.java
@@ -481,7 +481,7 @@ public class PlayState extends GameState {
 		camera.zoom = zoom;
 		sprite.zoom = zoom;
 		if (cameraTarget != null) {
-			if (cameraTarget.getBody() != null && cameraTarget.isAlive()) {
+			if (cameraTarget.getBody() != null) {
 				CameraStyles.lerpToTarget(camera, cameraTarget.getPosition().scl(PPM));
 				CameraStyles.lerpToTarget(sprite, cameraTarget.getPosition().scl(PPM));
 			}
